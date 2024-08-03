096609ad4be43d0ad528936f4b6c5796727bacea
alexbgh1 <alex.garnica@alumnos.uach.cl>
2024-08-03 11:06:18 -0400

    [pipeline]: implemented CI

--- app-cdk/lib/pipeline-cdk-stack.ts
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -27,6 +27,7 @@ export class PipelineCdkStack extends Stack {
         privileged: true,
         computeType: codebuild.ComputeType.LARGE,
       },
+      buildSpec: codebuild.BuildSpec.fromSourceFilename("buildspec_test.yml"),
     });
 
     // Define los artefactos
