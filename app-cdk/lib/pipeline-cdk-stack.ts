58cc012997c2cfd97af3623ce1732a4bbd5ff1b2
alexbgh1 <alex.garnica@alumnos.uach.cl>
2024-08-10 11:04:46 -0400

    CICD Dashboard sucess and failure widget

--- app-cdk/lib/pipeline-cdk-stack.ts
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -1,15 +1,15 @@
 import * as cdk from "aws-cdk-lib";
-import * as codepipeline from "aws-cdk-lib/aws-codepipeline";
-import * as codepipeline_actions from "aws-cdk-lib/aws-codepipeline-actions";
-import * as codebuild from "aws-cdk-lib/aws-codebuild";
-// import * as secretsmanager from "aws-cdk-lib/aws-secretsmanager";
+import { Stack, StackProps, CfnOutput, Duration } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import * as iam from "aws-cdk-lib/aws-iam";
-import { Stack, StackProps, CfnOutput } from "aws-cdk-lib";
-import * as ecr from "aws-cdk-lib/aws-ecr";
-import * as ecsPatterns from "aws-cdk-lib/aws-ecs-patterns";
+import * as codepipeline from "aws-cdk-lib/aws-codepipeline";
+import * as codebuild from "aws-cdk-lib/aws-codebuild";
 import * as codedeploy from "aws-cdk-lib/aws-codedeploy";
+import * as codepipeline_actions from "aws-cdk-lib/aws-codepipeline-actions";
+import * as ecr from "aws-cdk-lib/aws-ecr";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as ecsPatterns from "aws-cdk-lib/aws-ecs-patterns";
 import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import * as cloudwatch from "aws-cdk-lib/aws-cloudwatch";
 
 interface ConsumerProps extends StackProps {
   ecrRepository: ecr.Repository;
@@ -171,6 +171,33 @@ export class PipelineCdkStack extends Stack {
     new CfnOutput(this, "GitHubRepositoryUrl", {
       value: "https://github.com/alexbgh1/cicd-workshop",
       description: "URL del repositorio de GitHub usado como origen",
+    });
+
+    const buildRate = new cloudwatch.GraphWidget({
+      title: "Build Successes and Failures",
+      width: 6,
+      height: 6,
+      view: cloudwatch.GraphWidgetView.PIE,
+      left: [
+        new cloudwatch.Metric({
+          namespace: "AWS/CodeBuild",
+          metricName: "SucceededBuilds",
+          statistic: "sum",
+          label: "Succeeded Builds",
+          period: Duration.days(30),
+        }),
+        new cloudwatch.Metric({
+          namespace: "AWS/CodeBuild",
+          metricName: "FailedBuilds",
+          statistic: "sum",
+          label: "Failed Builds",
+          period: Duration.days(30),
+        }),
+      ],
+    });
+    new cloudwatch.Dashboard(this, "CICD_Dashboard", {
+      dashboardName: "CICD_Dashboard",
+      widgets: [[buildRate]],
     });
   }
 }
